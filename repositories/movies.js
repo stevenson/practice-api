1031237cca0f67ec4d6ee5e76ce9aab9f1bb6053
stevenson lee <stevenson.cunanan@gmail.com>
2022-05-16 17:54:26 +0800

    Update: use psuedo database in pastebin to simulate "retrieval"

--- repositories/movies.js
+++ repositories/movies.js
@@ -1,52 +1,64 @@
-function retrieveMany() {
-  const data = [
-    {
-      "name": "Moonlight",
-      "rating": 98,
-      "genres": [
-        "Drama"
-      ],
-      "showings": [
-        "18:30:00+11:00",
-        "20:30:00+11:00"
-      ]
-    },
-    {
-      "name": "Zootopia",
-      "rating": 92,
-      "genres": [
-        "Action & Adventure",
-        "Animation",
-        "Comedy"
-      ],
-      "showings": [
-        "19:00:00+11:00",
-        "21:00:00+11:00"
-      ]
-    },
-    {
-      "name": "The Martian",
-      "rating": 92,
-      "genres": [
-        "Science Fiction & Fantasy"
-      ],
-      "showings": [
-        "17:30:00+11:00",
-        "19:30:00+11:00"
-      ]
-    },
-    {
-      "name": "Shaun The Sheep",
-      "rating": 80,
-      "genres": [
-        "Animation",
-        "Comedy"
-      ],
-      "showings": [
-        "19:00:00+11:00"
-      ]
-    }
-  ];
+const axios = require('axios');
+
+async function retrieveBaseData() {
+  const response = await axios.get('https://pastebin.com/raw/cVyp3McN');
+  if (response.data) {
+    return response.data;
+  } else {
+    return [
+      {
+        "name": "Moonlight",
+        "rating": 98,
+        "genres": [
+          "Drama"
+        ],
+        "showings": [
+          "18:30:00+11:00",
+          "20:30:00+11:00"
+        ]
+      },
+      {
+        "name": "Zootopia",
+        "rating": 92,
+        "genres": [
+          "Action & Adventure",
+          "Animation",
+          "Comedy"
+        ],
+        "showings": [
+          "19:00:00+11:00",
+          "21:00:00+11:00"
+        ]
+      },
+      {
+        "name": "The Martian",
+        "rating": 92,
+        "genres": [
+          "Science Fiction & Fantasy"
+        ],
+        "showings": [
+          "17:30:00+11:00",
+          "19:30:00+11:00"
+        ]
+      },
+      {
+        "name": "Shaun The Sheep",
+        "rating": 80,
+        "genres": [
+          "Animation",
+          "Comedy"
+        ],
+        "showings": [
+          "19:00:00+11:00"
+        ]
+      }
+    ];
+  }
+}
+
+async function retrieveMany() {
+
+  const data = await retrieveBaseData();
 
   return data;
 }
